1a27f82ff5841512e016435f3c90efa781ec5a69
Charfeddine Hajlaoui <chajlaoui@wyplay.com>
2025-08-21 15:03:00 +0100

    init search screen

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,10 +1,31 @@
+import MovieCard from "@/components/MovieCard";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { useRouter } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { FlatList, Image, View } from "react-native";
 
 const Search = () => {
+  const router = useRouter;
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: "" }));
   return (
-    <View>
-      <Text>Search</Text>
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      />
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        numColumns={2}
+      />
     </View>
   );
 };
